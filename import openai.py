0c4dafe88614e543c8ad10dd1c95433525aca369
Sambosis <sambosis@gmail.com>
2023-04-08 22:33:15 -0400

    fork

---
+++ import openai.py
@@ -0,0 +1,30 @@
+import openai
+var processinput = function(textin){
+ fetch( `https://api.openai.com/v1/chat/completions`,
+ {
+   body: JSON.stringify({
+     "model": "gpt-3.5-turbo",
+       "messages": [
+         {role: "system", content: "You are the chatbot of a website called moleculARweb, which provides educational material for chemistry using commodity augmented reality. You answer questions about the website, about chemistry, science, etc."},
+         {role: "user", content: "What is the formula of acetic acid?"},
+         {role: "assistant", content: "The formula of acetic acid is CH3COOH"},
+         {role: "user", content: textin}
+       ],
+       "temperature": 0.3,
+       "max_tokens": 2000
+     }), 
+     method: "POST",
+     headers: {
+     "content-type": "application/json",
+     Authorization: "Bearer " + apikey,
+   }
+ }).then((response) => {
+   console.log(response) //If you want to check the full response
+   if (response.ok) {
+     response.json().then((json) => {
+       console.log(json);  //If you want to check the response as JSON
+       console.log(json.choices[0].message.content) //HERE'S THE CHATBOT'S RESPONSE
+     });
+   }
+ });
+}
